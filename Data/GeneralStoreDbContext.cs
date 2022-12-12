c133d4e33a2a2e4f2926ab8c146d39022beb67ef
jordan <jojohersh@gmail.com>
2022-12-11 20:36:43 -0500

    scaffolded using entity framework

---
+++ Data/GeneralStoreDbContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using GeneralStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeneralStore.Data;
+
+public partial class GeneralStoreDbContext : DbContext
+{
+    public GeneralStoreDbContext()
+    {
+    }
+
+    public GeneralStoreDbContext(DbContextOptions<GeneralStoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:GeneralStoreDB");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Customer__3214EC0786ABA1E7");
+
+            entity.ToTable("Customers", "dev");
+
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC077BEE5143");
+
+            entity.ToTable("Products", "dev");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Transaction>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Transact__3214EC07FF78675D");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Transactions)
+                .HasForeignKey(d => d.CustomerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Transacti__Custo__4222D4EF");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.Transactions)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Transacti__Produ__412EB0B6");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
